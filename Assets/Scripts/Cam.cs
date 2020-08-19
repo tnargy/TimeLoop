4257d4b88105755f1db4e1c63be8bd98c528fa44
tnargy-Desktop <clsscmstng@gmail.com>
2020-08-19 17:07:27 -0500

    Idle and movement

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,40 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            if (Input.GetMouseButton(1))
+            {
+                return Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        if (axisName == "Mouse Y")
+        {
+            if (Input.GetMouseButton(1))
+            {
+                return Input.GetAxis("Mouse Y");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        return Input.GetAxis(axisName);
+    }
+}
