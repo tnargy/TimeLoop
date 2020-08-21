bd2b9cbb92f6c28d2c882a83b7320dcc522fb56e
tnargy-Desktop <clsscmstng@gmail.com>
2020-08-21 13:13:17 -0500

    Added Player, moveing platform, and respawn on death.

---
+++ Assets/Death.cs
@@ -0,0 +1,25 @@
+﻿using Invector.vCharacterController;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    GameObject player;
+    GameObject spawnLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        spawnLocation = GameObject.Find("Spawn");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+            other.transform.SetPositionAndRotation(spawnLocation.transform.position, spawnLocation.transform.rotation);
+            other.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,83 @@
+﻿using Invector.vCharacterController;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    GameObject player;
+    Vector3 current_target;
+    int waypointIndex;
+
+    public Vector3[] waypoints;
+    public bool automatic;
+    public float moveSpeed, delay_time, tolerance, delay_start;
+
+
+    void Start()
+    {
+        if (waypoints == null || waypoints.Length == 0)
+        {
+            waypoints = new Vector3[1] { new Vector3(transform.position.x, transform.position.y, transform.position.z) };
+        }
+
+        current_target = waypoints[waypointIndex];
+        tolerance = moveSpeed * Time.deltaTime;
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        if (transform.position != current_target)
+        {
+            MovePlatform();
+        }
+        else
+        {
+            UpdateTarget();
+        }
+    }
+
+    private void MovePlatform()
+    {
+        Vector3 heading = current_target - transform.position;
+        transform.position += (heading / heading.magnitude) * moveSpeed * Time.deltaTime;
+        if (heading.magnitude < tolerance)
+        {
+            transform.position = current_target;
+            delay_start = Time.time;
+        }
+    }
+    private void UpdateTarget()
+    {
+        if (automatic)
+        {
+            if (Time.time - delay_start > delay_time)
+            {
+                NextPlatform();
+            }
+        }
+    }
+
+    public void NextPlatform()
+    {
+        waypointIndex = (waypointIndex + 1) % waypoints.Length;
+        current_target = waypoints[waypointIndex];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+            other.gameObject.GetComponent<vThirdPersonController>().useRootMotion = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+            other.gameObject.GetComponent<vThirdPersonController>().useRootMotion = false;
+        }
+    }
+
+}
