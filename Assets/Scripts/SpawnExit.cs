9564be0d401b880f2045ef40fd4a0435a2e0243f
tnargy-Desktop <clsscmstng@gmail.com>
2020-08-22 11:57:25 -0500

    When collecting trophy it disapears and turns on Exit

---
+++ Assets/Scripts/SpawnExit.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpawnExit : MonoBehaviour
+{
+    public void EnableExit()
+    {
+        transform.GetComponent<CapsuleCollider>().enabled = true;
+        transform.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Debug.Log("Winner");
+    }
+}
---
+++ Assets/Scripts/Trophy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Trophy : MonoBehaviour
+{
+    [SerializeField] public UnityEvent CollectTrophy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CollectTrophy.Invoke();
+        }
+    }
+
+    public void Cleanup()
+    {
+        Destroy(gameObject);
+    }
+}
