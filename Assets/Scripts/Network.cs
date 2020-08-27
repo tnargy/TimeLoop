8fedb91af32605a55a3af61c9ff1050c3b4ed945
tnargy-Desktop <clsscmstng@gmail.com>
2020-08-27 12:10:00 -0500

    Connected to us server!

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,17 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Network : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log($"We are connected to the { PhotonNetwork.CloudRegion} server!");
+    }
+}
